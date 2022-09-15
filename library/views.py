14be4a7c34b5f61e8ebf2493874c63ca01d7e0a0
Rutuja Gurav <rutujagurav72@gmail.com>
2022-09-16 01:40:22 +0530

    navigation added

--- library/views.py
+++ library/views.py
@@ -1,3 +1,16 @@
+from urllib import request
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def say_hello(request):
+    return render(request,'welcome.html')
+
+
+def admin(request):
+    return render(request,'admin_login.html')
+
+def student(request):
+    return render(request,'Student.html')
