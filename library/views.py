b78f4e0f6b24581661d4f564f0aa906fd00f99ca
Rutuja Gurav <rutujagurav72@gmail.com>
2022-09-16 12:14:18 +0530

    Attach user database with login register page

--- library/views.py
+++ library/views.py
@@ -1,9 +1,18 @@
+from sqlite3 import Cursor
 from urllib import request
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+
+import mysql.connector as sql
+
+name=''
+email=''
+pwd=''
+type=''
+
 
 
 # Create your views here.
@@ -13,32 +22,86 @@ def say_hello(request):
 
 
 def admin(request):
+    if request.method == 'POST':
+        m=sql.connect(host="localhost",user="root",passwd="new_password",database ="library_management")
+        cursor=m.cursor()
+        Email= request.POST['email']
+        password = request.POST['password']
+        c= "select * from users where email ='{}' and password = '{}' ".format(Email,password)
+        cursor.execute(c)
+        t = tuple(cursor.fetchall())
+        
+        print("DONE")
+        if t!=():
+            return render(request,'success.html')
+
+    # global email,pwd
+    # if request.method == 'POST':
+    #     m=sql.connect(host="localhost",user="root",passwd="new_password",database ="library_management")
+    #     cursor=m.cursor()
+    #     d=request.POST
+    #     for key,value in d.items():
+    #         if key == "email":
+    #             email=value
+    #         if key == "password1":
+    #             pwd=value
+
+    #         i= "select * from users where email='{}' and password='{}'".format(email,pwd)
+    #         cursor.execute(i)
+    #         t = tuple(cursor.fetchall())
+           
+     
+
     return render(request,'admin_login.html')
 
+
 def student(request):
+
+
+
+
     return render(request,'Student.html')
 
 def admin_register(request):
+    global name,email,pwd
     if request.method == 'POST':
-        name = request.POST['fullName']
-        email= request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        user = User.query.filter_by(email=email).first()
-        if user:
-            messages.info(request,'Email Already Exists!',category='error')
-        elif len(email) < 4:
-            messages.info(request,'Email must be greater than 3 characters.', category='error')
-        elif len(name) < 3:
-            messages.info(request,'Full Name must be greater than 2 characters.', category='error')
-        elif password1 != password2:
-            messages.info(request,'Passwords Don\'t Match.', category='error')
-        elif len(password1) < 8:
-            messages.info(request,'Password must be at least 8 characters.', category='error')
-        else:
-            user = User.objects.create_user(name=name, password=password2, email=email )
-            user.save()
-            messages.info(request,'User Created ')
-        return redirect('/')
+        m=sql.connect(host="localhost",user="root",passwd="new_password",database ="library_management")
+        cursor=m.cursor()
+        S=request.POST
+        for key,value in S.items():
+            if key == "type":
+                type=value
+            if key == "fullName":
+                name=value
+            if key == "email":
+                email=value
+            if key == "password1":
+                pwd=value
+        c= "insert into users Values ('{}','{}','{}','{}')".format(name,email,pwd,type)
+        cursor.execute(c)
+        m.commit()
+
+
+
+        # name = request.POST['fullName']
+        # email= request.POST['email']
+        # password1 = request.POST['password1']
+        # password2 = request.POST['password2']
+        # user = User.query.filter_by(email=email).first()
+        # if name:
+        #     messages.info(request,'Email Already Exists!',category='error')
+        # elif len(email) < 4:
+        #     messages.info(request,'Email must be greater than 3 characters.', category='error')
+        # elif len(name) < 3:
+        #     messages.info(request,'Full Name must be greater than 2 characters.', category='error')
+        # elif password1 != password2:
+        #     messages.info(request,'Passwords Don\'t Match.', category='error')
+        # elif len(password1) < 8:
+        #     messages.info(request,'Password must be at least 8 characters.', category='error')
+        # else:
+        #     user = User.objects.create_user(name=name, password=password2, email=email )
+        #     user.save()
+        #     messages.info(request,'User Created ')
+        # return redirect('/')
 
     return render(request,'admin_register.html')
